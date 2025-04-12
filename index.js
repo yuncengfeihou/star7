f5122a3a0ddc0f8674adde133ec9e3a3555590fe
yuncengfeihou <117951839+yuncengfeihou@users.noreply.github.com>
2025-04-12 23:18:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1083,7 +1083,8 @@ jQuery(async () => {
             /* --- 新增：返回按钮样式 --- */
             #${returnButtonId} {
                 display: inline-block; /* 让按钮占据一行 */ 
-                margin: 10px auto; /* 上下边距10px，左右自动居中 */
+                width: auto;
+                margin: 10px; /* 上下边距10px，左右自动居中 */
                 padding: 8px 15px; /* 内边距 */
                 /* 可以根据需要添加更多样式，例如背景色、边框等 */
                 background-color: var(--SmartThemeBtnBg);
